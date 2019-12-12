70e34f84e505ebb4daad2cdda3ab32f47cdd2049
JeffJones6912 <54726225+JeffJones6912@users.noreply.github.com>
2019-12-11 23:47:03 -0500

    Create Listing.java

---
+++ Ch.9/Listing.java
@@ -0,0 +1,30 @@
+public class Listing
+{
+    private int value;
+    boolean pushed;
+    public Listing(int v)
+    {
+        value = v;
+    }
+    public String toString()
+    {
+        return ("Vertex: " + value + "\n");
+    }
+    public Listing deepCopy()
+    {
+        Listing clone = new Listing(value);
+        return clone;
+    }
+    public boolean getPushed()
+    {
+        return pushed;
+    }
+    public void setPushed(boolean value)
+    {
+        pushed = value;
+    }
+    public void traversed()
+    {
+        System.out.println(this);
+    }
+}
